4dd8e1590efb8cc00ff686114bc363ef206dcfc5
MeannKyeopta <ragmacmeann@gmail.com>
2024-11-27 08:14:15 +0800

    2nd commit

---
+++ models/taskModel.js
@@ -0,0 +1,16 @@
+// models/Task.js
+let task = [
+    { id: 1, task: "Learn HTTP Methods", status: "Incomplete" },
+    { id: 2, task: "Organize Project Structure", status: "Complete" },
+  ];
+  
+  module.exports = task;
+
+  // models/itemModel.js
+let items = [
+    { id: 1, name: "Product A", price: 10 },
+    { id: 2, name: "Product B", price: 20 },
+    { id: 3, name: "Product C", price: 30 },
+  ];
+  
+  module.exports = items;
